6c648417ca8f56dc424b94c262b8ee50692b46be
Tejeshreddy <s565694@nwmissouri.edu>
2023-10-31 21:48:35 -0500

    modified index.js

--- routes/dress.js
+++ routes/dress.js
@@ -3,7 +3,7 @@ var router = express.Router();
  
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dress', { title: 'Search Results dress' });
+  res.render('dress', { title: 'Search Results for dress' });
 });
  
 module.exports = router;
